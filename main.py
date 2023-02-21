5bbb2d106d23bef285bcb7d6e23e6b3a6abe115f
Gokulapps <61132761+Gokulapps@users.noreply.github.com>
2023-02-21 18:09:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def fit_model(model, device, trainloader, testloader, l1=False, l2=False):
     scheduler.step()
     test(model, device, testloader, epoch)
 
-fit_model(model, device, train_loader, test_loader, True, False)
+fit_model(model, device, train_loader, test_loader, False, True)
 print('Model Saved')
 print('Plotting Graphs')
 plot_graph(test_loss, test_acc, fig_size=(15,10))
