7928f311d4e2118491f2f63aea4d9d39997057fd
Gokulapps <61132761+Gokulapps@users.noreply.github.com>
2023-02-23 17:23:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,8 +90,7 @@ def define_transforms(aug=True, dataset_mean=(0.5, 0.5, 0.5), dataset_std=(0.5, 
         if aug:
             augmentation = A.Compose([A.PadIfNeeded(min_height=36, min_width=36, p=1),
                                       A.RandomCrop(width=32, height=32),
-                                      A.OneOf([A.Flip(p=1, flip_code=1),            # horizontal flip
-                                               A.Flip(p=1, flip_code=0)], p=0.5),   # vertical flip
+                                      A.OneOf([A.HorizontalFlip(p=1), A.VerticalFlip(p=1)], p=0.5),
                                       A.CoarseDropout(max_holes=1, max_height=8, max_width=8, fill_value=dataset_mean)])
             return transform, augmentation
         else:
