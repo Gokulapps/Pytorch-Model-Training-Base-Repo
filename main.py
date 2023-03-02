acf58146bf35522eaf7adf95b026645adc4bb923
Gokulapps <61132761+Gokulapps@users.noreply.github.com>
2023-03-02 19:27:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,11 +49,13 @@ test_acc = []
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # model = ResNet18().to(device)
 model= CustomResnet().to(device)
-model_exp = deepcopy(model)
+model_exp1 = deepcopy(model)
+model_exp2 = deepcopy(model)
 if device == 'cuda':
     print("=> Parallelizing Training across Multiple GPU's")
     model = torch.nn.DataParallel(model)
-    model_exp = torch.nn.DataParallel(model_exp)
+    model_exp1 = torch.nn.DataParallel(model_exp1)
+    model_exp2 = torch.nn.Dataparallel(model_exp2)
 
 class torchvisionDataset(dataset_class):
     def __init__(self, root='./data', train=True, download=True, transform=transforms.ToTensor(), alb_aug=None):
@@ -109,6 +111,10 @@ train_dataset = torchvisionDataset(root='./data', train=True, download=True, tra
 test_dataset =  torchvisionDataset(root='./data', train=False, download=True, transform=transforms[0], alb_aug=None)
 train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2, pin_memory = True)
 test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2, pin_memory = True)
+optimizer_exp1 = optim.SGD(model_exp1.parameters(), lr=0.0001, momentum=0.9)
+max_lr = find_optimal_lr(model_exp1, device, train_loader, optimizer_exp1, nn.CrossEntropyLoss(), end_lr=4)
+optimizer_exp2 = optim.SGD(model_exp2.parameters(), lr=max_lr/10, momentum=0.9)
+final_max_lr = find_optimal_lr(model_exp2, device, train_loader, optimizer_exp2, nn.CrossEntropyLoss(), end_lr=max_lr*10)
 
 def train(model, device, train_loader, optimizer, l1_reg, scheduler):
     try:
@@ -132,6 +138,7 @@ def train(model, device, train_loader, optimizer, l1_reg, scheduler):
             train_loss.append(loss)
             loss.backward()
             optimizer.step() 
+            scheduler.step()
             pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
             processed += len(images)
@@ -143,7 +150,7 @@ def train(model, device, train_loader, optimizer, l1_reg, scheduler):
         print(f'Error in {train.__name__} Block')
    
 
-def test(model, device, test_loader, epoch):
+def test(model, device, test_loader, epoch, optimizer):
     try:
         global best_acc, test_loss, test_acc
         model.eval()
@@ -170,6 +177,7 @@ def test(model, device, test_loader, epoch):
                 os.mkdir('checkpoint')
             state = {
               'model': model.state_dict(),
+              'optimizer': optimizer.state_dict(),
               'accuracy': accuracy,
               'epoch': epoch,
               #'optimizer': optimizer.state_dict()
@@ -181,27 +189,25 @@ def test(model, device, test_loader, epoch):
         print(e)
         print(f'Error in {test.__name__} Block')
 
-def fit_model(model, model_exp, device, trainloader, testloader, l1=False, l2=False):
+def fit_model(model, final_max_lr, device, trainloader, testloader, l1=False, l2=False):
     try:
         global best_acc, Epochs, test_loss, test_acc
         if l2:
             optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
         else:
             optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
-        optimizer_exp = optim.SGD(model_exp.parameters(), lr=args.lr, momentum=0.9)
-        max_lr = find_optimal_lr(model_exp, device, train_loader, optimizer_exp, nn.CrossEntropyLoss(), end_lr=4)
         steps_per_epoch = len(train_loader) 
         total_steps = steps_per_epoch * Epochs
         step_size_up = steps_per_epoch * 5
         step_size_down = total_steps - step_size_up
-        scheduler = OneCycleLR(optimizer, max_lr=max_lr, epochs=Epochs, total_steps=total_steps, steps_per_epoch=steps_per_epoch, pct_start=step_size_up/total_steps, div_factor=10, three_phase=False, final_div_factor=50)
+        scheduler = OneCycleLR(optimizer, max_lr=final_max_lr, epochs=Epochs, total_steps=total_steps, steps_per_epoch=steps_per_epoch, pct_start=step_size_up/total_steps, div_factor=10, three_phase=False, final_div_factor=50)
         if args.resume:
             # Load checkpoint.
             print('==> Resuming from checkpoint..')
             assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
             checkpoint = torch.load('./checkpoint/model_state.pth')
             model.load_state_dict(checkpoint['model'])
-            #optimizer.load_state_dict(checkpoint['optimizer'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
             best_acc = checkpoint['accuracy']
             start_epoch = checkpoint['epoch']
         else:
@@ -210,13 +216,12 @@ def fit_model(model, model_exp, device, trainloader, testloader, l1=False, l2=Fa
         for epoch in range(start_epoch, Epochs+1):
             print("EPOCH:", epoch)
             train(model, device, trainloader, optimizer, l1, scheduler)
-            scheduler.step()
-            test(model, device, testloader, epoch)
+            test(model, device, testloader, epoch, optimizer)
     except Exception as e:
         print(e)
         print(f'Error in {fit_model.__name__} Block')
 
-fit_model(model, model_exp, device, train_loader, test_loader, False, False)
+fit_model(model, final_max_lr, device, train_loader, test_loader, False, False)
 print('Model Saved')
 print('Plotting Graphs')
 plot_graph(test_loss, test_acc, fig_size=(15,10))
